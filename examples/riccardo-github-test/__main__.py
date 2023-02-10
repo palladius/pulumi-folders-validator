99862d18398d002e3c9b2f3ce0f26074d703242a
Riccardo Carlesso (Derek) <palladiusbonton@gmail.com>
2023-02-10 12:36:15 +0100

    boh

--- examples/riccardo-github-test/__main__.py
+++ examples/riccardo-github-test/__main__.py
@@ -4,17 +4,18 @@ import pulumi
 from pulumi_gcp import storage
 import pulumi_github as github
 
+# Create a GCP resource (Storage Bucket)
+bucket = storage.Bucket('pulumi-github-test', location="EU")
+
+# Export the DNS name of the bucket
+pulumi.export('bucket_name', bucket.url)
+
 # API Doc here: https://www.pulumi.com/registry/packages/github/api-docs/repository/
 repo = github.Repository("pulumized-demo-repo",
   description="Generated from Pulumi from pulumi-folders-validator repo.",
   visibility="private",
 )
 
-# Create a GCP resource (Storage Bucket)
-bucket = storage.Bucket('pulumi-github-test', location="EU")
-
-# Export the DNS name of the bucket
-pulumi.export('bucket_name', bucket.url)
 pulumi.export('gh_repo_name', repo.full_name)
 
 #carlessian_url = lambda repo: f"https://github.com/{repo.full_name}"
